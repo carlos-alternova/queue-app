013321d8141c52a92b4f241228b54fd62aed98b7
Carlos Osorio <carlos.osorio@alternova.com>
2025-09-06 12:23:43 -0500

    Add logic into new-ticket layout

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,16 @@
+const lblNewTicket = document.getElementById('lbl-new-ticket')
+const btnNewTicket = document.querySelector('button')
 
+const getLastTicket = async () => {
+  // send GET request to "/api/ticket/last"
+  const data = await fetch('/api/ticket/last').then((resp) => resp.json())
 
-console.log('Nuevo Ticket HTML');
+  lblNewTicket.innerText = `Ticket: ${data}`
+}
+
+btnNewTicket.addEventListener('click', async () => {
+  await fetch('/api/ticket/', { method: 'POST' })
+  getLastTicket()
+})
+
+getLastTicket()
