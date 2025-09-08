98836a20feced5f389b94cc21c2a94a50f9038c4
Carlos Osorio <carlos.osorio@alternova.com>
2025-09-08 17:00:55 -0500

    Set title in desk page

--- public/js/desk.js
+++ public/js/desk.js
@@ -1,6 +1,7 @@
 import connectToWebSockets from './socket-client.js'
 
 // HTML references:
+const deskTitle = document.querySelector('h1')
 const pendingText = document.getElementById('lbl-pending')
 
 const getPendingTickets = async () => {
@@ -11,5 +12,25 @@ const getPendingTickets = async () => {
   pendingText.innerText = `Pending: ${pendingTickets.length || 0}`
 }
 
+const getDeskName = () => {
+  // My method:
+  // const url = document.baseURI
+  // const params = url.split('?').pop()
+  // const paramsArray = params.split('&').map((pair) => pair.split('='))
+  // const paramsMap = new Map(paramsArray)
+  // const deskName = paramsMap.get('deskName')
+
+  const params = window.location.search
+  const extractedParams = new URLSearchParams(params)
+
+  if (!extractedParams.has('deskName')) {
+    window.location = 'index.html'
+    throw new Error('deskName param is required')
+  }
+
+  deskTitle.innerText = extractedParams.get('deskName')
+}
+
 getPendingTickets()
+getDeskName()
 connectToWebSockets(pendingText)
